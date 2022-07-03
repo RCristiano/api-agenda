bfeed9377e310955f737571431cca750fc8edafa
Rodrigo Cristiano <rcristianofv@hotmail.com>
2022-07-03 14:52:07 -0300

    ✨ Add base model classes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from enum import Enum
 import os
 import mimesis
 from fastapi import FastAPI, Request, HTTPException, status
@@ -39,19 +41,98 @@ def read_root(request: Request):
     }
 
 
+class Status(Enum):
+    INATIVO = 0
+    ATIVO = 1
+
+
+class Application(BaseModel):
+    id: int
+    owner_id: int
+    pet_id: int
+    status: Status
+    description: str
+    # +insert_application(pet_id, status, description)
+    # +get_application(id)
+    # +update_application(id, status, description)
+    # +status_update(id, status)
+
+
+@app.post("/application", summary="Request a application", tags=["Application"])
+def submit_application(application: Application):
+    return application
+
+
+@app.get("/applications/{application_id}", dependencies=[Security(validate_jwt)], summary="Get application", tags=["Application"])
+def get_application(application_id: int):
+    fake = mimesis.Person()
+    return Owner(id=application_id, name=fake.full_name(), age=fake.age())
+
+
+@app.get("/applications", dependencies=[Security(validate_jwt)], summary="Get all application", tags=["Application"])
+def get_application():
+    fake = mimesis.Person()
+    return Owner(id=application_id, name=fake.full_name(), age=fake.age())]
+
 class Owner(BaseModel):
     id: int
-    name: str
-    age: int
+    cpf: int
+    password: str
+    Name: str
+    Email: str
+    Phone: str
+    Address: str
+    City: str
+    # +get_owner(id)
+    # +get_pets(id)
+    # +add_owner(name, email, phone, address, city)
+    # +update_owner(id, name, email, phone, address, city)
 
 
-@app.get("/owners", dependencies=[Security(validate_jwt)], summary="Get all owners", tags=["Owners"])
+@ app.get("/owners", dependencies=[Security(validate_jwt)], summary="Get all owners", tags=["Owner"])
 def list_owners():
-    owner = mimesis.Person()
+    owner= mimesis.Person()
     return [Owner(id=i, name=owner.full_name(), age=owner.age()) for i in range(1, 5)]
 
 
-@app.get("/owners/{owner_id}", dependencies=[Security(validate_jwt)], summary="Get Owner", tags=["Owner"])
+@ app.get("/owners/{owner_id}", dependencies=[Security(validate_jwt)], summary="Get Owner", tags=["Owner"])
 def get_owner(owner_id: int):
-    fake = mimesis.Person()
+    fake= mimesis.Person()
     return Owner(id=owner_id, name=fake.full_name(), age=fake.age())
+
+
+class Pet(BaseModel):
+    id: int
+    owner_id: int
+    Name: str
+    birth: datetime
+    species: str
+    # +getPet(id)
+    # +add_pet(owner_id, name)
+    # +update_pet(id, owner_id, name)
+    # +get_charts(id)
+
+
+class Chart(BaseModel):
+    id: int
+    pet_id: int
+    # +get_chart(id)
+    # +add_chart(pet_id, vet_id, date, time, description)
+    # +update_chart(id, pet_id, vet_id, date, time, description)
+
+
+class Exam(BaseModel):
+    id: int
+    chart_id: int
+    Doctor: str
+    execution_date: datetime
+    type: str
+    file_link: str
+    # +insert(chart_id, Doctor, execution_date, type, file_link)
+
+
+class Pathology(BaseModel):
+    id: int
+    chart_id: int
+    description: str
+    # +insert(chart_id, description)
