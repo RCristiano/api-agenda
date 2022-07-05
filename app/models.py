3c6ddf7b849f096b9a52916e017925df37fea1c6
Rodrigo Cristiano <rcristianofv@hotmail.com>
2022-07-04 23:07:31 -0300

    Remove Date type

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Date, Enum, ForeignKey, Integer, String
+from sqlalchemy import Column, Enum, ForeignKey, Integer, String
 
 from app.database import Base
 from app.schemas.application import Status
@@ -18,8 +18,8 @@ class Pet(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     owner_id = Column(Integer, ForeignKey("owners.id"))
-    Name = Column(String)
-    birth = Column(Date)
+    name = Column(String)
+    birth = Column(String)
     species = Column(String)
 
 
@@ -34,27 +34,3 @@ class Owner(Base):
     phone = Column(String)
     address = Column(String)
     city = Column(String)
-
-# class Chart(BaseModel):
-#     id: int
-#     pet_id: int
-#     # +get_chart(id)
-#     # +add_chart(pet_id, vet_id, date, time, description)
-#     # +update_chart(id, pet_id, vet_id, date, time, description)
-
-
-# class Exam(BaseModel):
-#     id: int
-#     chart_id: int
-#     Doctor: str
-#     execution_date: datetime
-#     type: str
-#     file_link: str
-#     # +insert(chart_id, Doctor, execution_date, type, file_link)
-
-
-# class Pathology(BaseModel):
-#     id: int
-#     chart_id: int
-#     description: str
-#     # +insert(chart_id, description)
