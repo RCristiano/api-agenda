7bb732dd60d7f67c1089b0560c795e3594249c97
Rodrigo Cristiano <rcristianofv@hotmail.com>
2022-07-05 02:37:11 -0300

    🐛 Fix connection with PostgreSQL on Heroku

--- app/database.py
+++ app/database.py
@@ -4,13 +4,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.environ.get(
-    "DATABASE_URL") or "sqlite:///./db.sqlite3"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL") \
+    or "sqlite:///./db.sqlite3"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace(
+        "postgres://", "postgresql://", 1)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
