f0f18dec22c3134f2f28ed6086642c3302f2d4c2
Rodrigo Cristiano <rcristianofv@hotmail.com>
2022-07-05 02:20:33 -0300

    ✨ Add conection with PostgreSQL on Heroku

--- app/database.py
+++ app/database.py
@@ -1,9 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite3"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    "DATABASE_URL") or "sqlite:///./db.sqlite3"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
